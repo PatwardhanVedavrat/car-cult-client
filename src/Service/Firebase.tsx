310e5a56fbd3d6c18dc9f17cd77fba698077e118
Vedavrat <vedavratpp@gmail.com>
2022-05-02 10:53:07 +0530

    Create car-cult website

---
+++ src/Service/Firebase.tsx
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+export const app = firebase.initializeApp({
+  projectId: "car-cult-79898",
+  appId: "1:683571606517:web:4043bc0ccbee2921fbe6bc",
+  storageBucket: "car-cult-79898.appspot.com",
+  locationId: "asia-northeast3",
+  apiKey: "AIzaSyDR_OhohSyYzWAMD4p5sB-5HKpGaiS_-oc",
+  authDomain: "car-cult-79898.firebaseapp.com",
+  messagingSenderId: "683571606517",
+  measurementId: "G-CHTDHCD2KT",
+});
